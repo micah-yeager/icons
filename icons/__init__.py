a2c288b4b9ec49258488431d17ef546d347182da
Micah Yeager <micah@yeagers.co>
2023-01-18 21:42:14 -0500

    Clean up environment

--- icons/__init__.py
+++ icons/__init__.py
@@ -2,5 +2,4 @@ from icons.inputs import input_provider
 from icons.outputs import output_provider
 from icons.sources import source_provider
 
-
 __all__ = ['source_provider', 'input_provider', 'output_provider']
